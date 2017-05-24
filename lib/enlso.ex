d0635f1f5d8d78a80e4f69ddb8826e0cec6d73ac
cltian <tianchunlin123@gmail.com>
2017-05-24 14:05:34 +0800

    init

---
+++ lib/enlso.ex
@@ -0,0 +1,18 @@
+defmodule Enlso do
+  @moduledoc """
+  Documentation for Enlso.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Enlso.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enlso_test.exs
@@ -0,0 +1,8 @@
+defmodule EnlsoTest do
+  use ExUnit.Case
+  doctest Enlso
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
