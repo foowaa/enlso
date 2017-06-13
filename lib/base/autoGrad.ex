1de262a0878c2272c910c54947f332c7e1bf8eec
cltian <tianchunlin123@gmail.com>
2017-06-13 10:14:46 +0800

    add files

---
+++ lib/base/autoGrad.ex
@@ -0,0 +1,32 @@
+defmodule elsno.base do
+
+ @moduledoc """
+ An implementation of auto grad
+ """
+
+    defp finiteDiff(x) do
+    @doc """
+    finite differencing
+    """
+    end
+
+    defp centralDiff(x) do
+    @doc """
+    central differencing
+    """
+    end
+
+    defp norm(x) do
+    @doc """
+    l2 norm
+    """
+
+    end
+
+    def grad(x, type \\ "central") do
+    @doc """
+    the main part
+    """
+    end
+
+end
