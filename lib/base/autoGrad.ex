193b0ba05511ad0142bce2097316a42391f0fe65
cltian <tianchunlin123@gmail.com>
2017-06-22 16:43:27 +0800

    add autoHess and fix mang bugs

--- lib/base/autoGrad.ex
+++ lib/base/autoGrad.ex
@@ -9,63 +9,67 @@ defmodule elsno.base.grad do
     @doc """
     finite differencing
     """
-    p = length(x)
-    y = Enum.map(x, f)
-    mu = 2 * Math.sqrt(1.0e-12) * (1+norm(x))
-    diff = List.duplicate(0.0, p)
-    e_j = List.duplicate(1.0, p)
-    for j <- 0..p do
-        Enum.at(diff, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
-            Enum.map(fn(x) -> Enum.at(x, 0)*Enum.at(x, 1)) |>
-            Enum.zip(x) |> Enum.map(fn(x) -> Enum.at(x, 0)+Enum.at(x, 1)) |> f
-    end
-    mu_list = List.duplicate(mu, p)
-    g = y |> List.duplicate(p) |> Enum.zip(diff) |> Enum.map(fn(x) -> Enum.at(x, 1)-Enum.at(x, 0)) |>
-        Enum.zip(mu_list) |> Enum.map(fn(x) -> Enum.at(x, 0)/Enum.at(x, 1))
+        p = length(x)
+        y = Enum.map(x, f)
+        mu = 2 * Math.sqrt(1.0e-12) * (1+norm(x))
+        diff = List.duplicate(0.0, p)
+        
+        for j <- 0..p do
+            e_j = List.duplicate(0.0, p)
+            Enum.at(e_j, j) = 1.0
+            Enum.at(diff, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> Enum.at(x, 0)*Enum.at(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 0)+elem(x, 1) end) |> f
+        end
+        mu_list = List.duplicate(mu, p)
+        g = y |> List.duplicate(p) |> Enum.zip(diff) |> Enum.map(fn(x) -> elem(x, 1)-elem(x, 0) end) |>
+            Enum.zip(mu_list) |> Enum.map(fn(x) -> elem(x, 0)/elem(x, 1) end)
     end
 
     defp centralDiff(f, x) do
     @doc """
     central differencing
     """
-    p = length(x)
-    mu = 2 * Math.sqrt(1.0e-12) * (1+norm(x))
-    diff1 = List.duplicate(0, p)
-    diff2 = List.duplicate(0, p)
-    e_j = List.duplicate(1.0, p)
-    for j <- 1..p do
-        Enum.at(diff1, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
-            Enum.map(fn(x) -> Enum.at(x, 0)*Enum.at(x, 1)) |>
-            Enum.zip(x) |> Enum.map(fn(x) -> Enum.at(x, 1)+Enum.at(x, 0)) |> f
-        Enum.at(diff2, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
-            Enum.map(fn(x) -> Enum.at(x, 0)*Enum.at(x, 1)) |>
-            Enum.zip(x) |> Enum.map(fn(x) -> Enum.at(x, 1)-Enum.at(x, 0)) |> f      
-    end
-    mu_list = List.duplicate(2*mu, p)
-    g = diff1 |> Enum.zip(diff2) |> Enum.map(fn(x) -> Enum.at(x, 0)-Enum.at(x, 1)) |> Enum.zip(mu_list) |>
-        Enum.map(fn(x) -> Enum.at(x, 0)/Enum.at(x, 1))
+        p = length(x)
+        mu = 2 * Math.sqrt(1.0e-12) * (1+norm(x))
+        diff1 = List.duplicate(0.0, p)
+        diff2 = List.duplicate(0.0, p)
+        
+        for j <- 1..p do
+            e_j = List.duplicate(0.0, p)
+            Enum.at(e_j, j) = 1.0
+            Enum.at(diff1, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> elem(x, 0)*elem(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 1)+elem(x, 0) end) |> f
+            Enum.at(diff2, j) = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> elem(x, 0)*elem(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 1)-elem(x, 0) end) |> f      
+        end
+        mu_list = List.duplicate(2*mu, p)
+        g = diff1 |> Enum.zip(diff2) |> Enum.map(fn(x) -> elem(x, 0)-elem(x, 1) end) |> Enum.zip(mu_list) |>
+            Enum.map(fn(x) -> elem(x, 0)/elem(x, 1) end)
     end
 
     def norm(x) do
     @doc """
     l2 norm
     """
-    Enum.reduce(x, &(&1*&1)) |> Math.sqrt
+         Enum.reduce(x, &(&1*&1)) |> Math.sqrt
     end
 
-    def grad(f, x, type \\ "central") do
+    def grad!(x, f, type \\ "central") do
     @doc """
     the main part
     """
-    cond do
-        String.equivalent?("central") ->
-            centralDiff(f, x)
-        String.equivalent?("finite") -> 
-            finiteDiff(f, x)
-        _ ->
-            raise ArgumentError, message:"type is invalid"
+        cond do
+            String.equivalent?(type, "central") ->
+                centralDiff(f, x)
+            String.equivalent?(type, "finite") -> 
+                finiteDiff(f, x)
+            _ ->
+                raise ArgumentError, message:"type is invalid"
 
-    end
+        end
     end
 
 end
--- lib/base/autoHess.ex
+++ lib/base/autoHess.ex
@@ -9,29 +9,64 @@ defmodule enlso.base.hess do
     @doc """
     finite differencing
     """
-    p = length(x)
-    mu = 2 * Math.sqrt(1.0e-12) * (1+Grad.norm(x))
-    
-    diff = List.duplicate(0, p) |> List.duplicate(p)
-    e_j = List.duplicate(0, p)
-    for j <- 1..p do
-        Enum.at(e_j, j) = 1.0
-        g = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
-            Enum.map(fn(x) -> Enum.at(x, 0)*Enum.at(x, 1)) |>
-            Enum.zip(x) |> Enum.map(fn(x) -> Enum.at(x, 0)+Enum.at(x, 1)) |> f
-        
-    end
+        p = length(x)
+        mu = 2 * Math.sqrt(1.0e-12) * (1+Grad.norm(x))
+        diff = List.duplicate(0.0, p) |> List.duplicate(p)
+
+        for j <- 1..p do
+            e_j = List.duplicate(0.0, p)
+            Enum.at(e_j, j) = 1.0
+            g = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> elem(x, 0)*elem(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 0)+elem(x, 1) end) |> Grad.grad(f,"finite")
+            for i <- 1..p, do: diff |> Enum.at(i) |> List.replace_at(j, g|>Enum.at(i))
+        end
+        rep_g = List.duplicate(0, p) |> List.duplicate(p)
+        for i <- 1..p, do: rep_g |> List.replace_at(i, g|>Enum.at(i)|>List.duplicate(p))
+        mu_list = List.duplicate(mu, p)
+        h = diff |> Enum.zip(rep_g) |> Enum.map(fn(x) -> Tuple.to_list(x) end) |> 
+                    Enum.map(fn(x) -> x|>Enum.at(0)|>Enum.zip(x|>Enum.at(1))|>Enum.map(fn(x)-> elem(x,0)-elem(x,1) end) end)
+        H = List.duplicate(0, p) |> List.duplicate(p)
+        for i <- 1..p, do: Enum.at(H, i) = Enum.at(h,i)|>Enum.zip(mu_list)|>Enum.map(fn(x) -> elem(x,0)/elem(x,1) end)
     end
 
     defp centralHess(f, x) do
     @doc """
     central differencing
     """    
+        p = length(x)
+        mu = 2 * Math.sqrt(1.0e-12) * (1+Grad.norm(x))
+        diff1 = List.duplicate(0.0, p) |> List.duplicate(p)
+        diff2 = List.duplicate(0.0, p) |> List.duplicate(p)
+        
+        for j<- 1..p do
+            e_j = List.duplicate(0.0, p)
+            Enum.at(e_j, j) = 1.0
+            g1 = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> elem(x, 0)*elem(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 0)+elem(x, 1) end) |> Grad.grad(f)         
+            g2 = mu |> List.duplicate(p) |> Enum.zip(e_j) |>
+                Enum.map(fn(x) -> elem(x, 0)*elem(x, 1) end) |>
+                Enum.zip(x) |> Enum.map(fn(x) -> elem(x, 1)-elem(x, 0) end) |> Grad.grad(f)
+        end
+        mu_list = List.duplicate(2*mu, p)
+        h = diff1 |> Enum.zip(diff2) |> Enum.map(fn(x) -> Tuple.to_list(x) end) |> 
+                    Enum.map(fn(x) -> x|>Enum.at(0)|>Enum.zip(x|>Enum.at(1))|>Enum.map(fn(x)-> elem(x,0)-elem(x,1) end) end)
+        H = List.duplicate(0, p) |> List.duplicate(p)
+        for i <- 1..p, do: Enum.at(H, i) = Enum.at(h,i)|>Enum.zip(mu_list)|>Enum.map(fn(x) -> elem(x,0)/elem(x,1) end)
     end
 
     def hess(f, x, type \\ "central") do
     @doc """
     the main part
     """    
+        cond do
+            String.equivalent?(type, "central") ->
+                centralHess(f, x)
+            String.equivalent?(type, "finite") ->
+                finiteHess(f, x)
+            _ ->
+                raise ArgumentError, message:"type is invalid"
+        end
     end
 end
